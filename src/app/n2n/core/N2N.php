c12dab601101d377a5eaadbcb7d674db52d7b2f7
Andreas von Burg <von-burg@hnm.ch>
2025-11-07 14:18:26 +0100

    n2n 7.5

--- src/app/n2n/core/N2N.php
+++ src/app/n2n/core/N2N.php
@@ -60,7 +60,7 @@ use n2n\cache\CharacteristicsList;
 define('N2N_CRLF', "\r\n");
 
 class N2N {
-	const VERSION = '7.4.4';
+	const VERSION = '7.5.0';
 	const LOG4PHP_CONFIG_FILE = 'log4php.xml'; 
 	const LOG_EXCEPTION_DETAIL_DIR = 'exceptions';
 	const LOG_MAIL_BUFFER_DIR = 'log-mail-buffer';
