fbd1b49a8bc7b1767061bb69c740772e4679d1c8
Andreas von Burg <von-burg@hnm.ch>
2025-11-14 23:32:03 +0100

    N2nBatch

--- src/app/n2n/core/ext/N2nBatch.php
+++ src/app/n2n/core/ext/N2nBatch.php
@@ -4,6 +4,10 @@ namespace n2n\core\ext;
 
 interface N2nBatch {
 
-	function trigger(?BatchTriggerConfig $config = null): void;
+	/**
+	 * @param BatchTriggerConfig|null $config
+	 * @return array arbitrary result object per successfully invoked batch job.
+	 */
+	function trigger(?BatchTriggerConfig $config = null): array;
 }
 
